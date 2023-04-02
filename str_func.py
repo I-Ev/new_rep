6ddc0a2fa36ed920e008172ce8f5ddc9e463af08
Ilya Evlanov <ilya_rm@mail.ru>
2023-04-02 13:18:19 +0300

    modified str_func (new def + docstring)

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def up_string(text):
+    """локальный docstring"""
     return text.upper()
+
+def title_string(text):
+    """Переводит первые буквы в верхний регистр"""
+    return text.capitalize()
